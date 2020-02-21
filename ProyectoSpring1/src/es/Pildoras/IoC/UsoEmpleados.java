25eb1d7724e5d0b4f763824f9488db1e1ac3ebde
demonics78 <jmartinez3310@alumno.uned.es>
2020-02-21 23:24:50 +0100

    Video 8

--- ProyectoSpring1/src/es/Pildoras/IoC/UsoEmpleados.java
+++ ProyectoSpring1/src/es/Pildoras/IoC/UsoEmpleados.java
@@ -1,16 +1,26 @@
 package es.Pildoras.IoC;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UsoEmpleados {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		/*
 		// Creacion de objectos de tipo Empleado
 		Empleados Empleado1 = new DirectorEmpleado();
 		
 		// Uso de los objetos creados
-		System.out.println(Empleado1.getTareas());
+		System.out.println(Empleado1.getTareas());*/
+		
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContent.xml");
+		
+		Empleados Juan =  contexto.getBean("miEmpleado",Empleados.class);
+		System.out.println(Juan.getTareas());
+		
+		contexto.close();
+		
 	}
 
 }
